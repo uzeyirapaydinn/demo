ffe8a1fdfb3cd925bb506b1e2193681940182789
QuickCode Auto Commit <uzeyirapaydin@gmail.com>
2024-12-08 17:11:36 -0500

    QuickCode.Net Auto Commit: 12/8/2024 10:11:36 PM

--- src/Services/QuickCode.Demo.EventListenerService/Models/ObjectToJObjectTypeConverter.cs
+++ src/Services/QuickCode.Demo.EventListenerService/Models/ObjectToJObjectTypeConverter.cs
@@ -9,7 +9,7 @@ public class ObjectToJObjectTypeConverter : IYamlTypeConverter
 {
     public bool Accepts(Type type) => type == typeof(JObject);
 
-    public object ReadYaml(IParser parser, Type type)
+    public object? ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
     {
         var deserializer = new DeserializerBuilder()
             .WithNamingConvention(CamelCaseNamingConvention.Instance)
@@ -19,7 +19,7 @@ public class ObjectToJObjectTypeConverter : IYamlTypeConverter
         return yamlObject == null ? new JObject() : JObject.FromObject(yamlObject);
     }
 
-    public void WriteYaml(IEmitter emitter, object value, Type type)
+    public void WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
     {
         throw new NotImplementedException();
     }
